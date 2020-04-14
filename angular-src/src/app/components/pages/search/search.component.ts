017848364adf2ac9b92d6bc21b572149fe15bdad
StriderX1X <rishabhkalathiya987@gmail.com>
2020-04-14 11:28:39 +0530

    added search component

--- angular-src/src/app/components/pages/search/search.component.ts
+++ angular-src/src/app/components/pages/search/search.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  selector: "app-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.css"]
 })
 export class SearchComponent implements OnInit {
+  searchForm: FormGroup;
+  x: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    this.searchForm = new FormGroup({
+      source: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^[A-Za-z -]+$/)
+      ]),
+      destination: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^[A-Za-z -]+$/)
+      ]),
+      doj: new FormControl(null, Validators.required)
+    });
   }
 
+  onSubmit() {
+    console.log(this.searchForm);
+    // this.searchForm.reset();
+  }
 }
